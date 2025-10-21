3ffbe5665dcdee950f4594fc308b0f5321e9e062
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-10-21 22:14:18 +0700

    update class bind

--- src/components/FilmCategory/index.js
+++ src/components/FilmCategory/index.js
@@ -2,40 +2,43 @@ import { useState, useEffect } from "react";
 import { getCategory } from "~/services";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLeftRight } from "@fortawesome/free-solid-svg-icons";
+import styles from "./FilmCategory.module.scss";
+// import classNames from "classnames/bind";
+import classNames from 'classnames/bind'
+
 
 function FilmCategory() {
+  const cx = classNames.bind(styles);
+
   const [categories, setCategories] = useState([]);
   useEffect(() => {
     const fetchApi = async () => {
-        const result = await getCategory()
-        setCategories(result.items)
-    }
+      const result = await getCategory();
+      setCategories(result.items);
+    };
     fetchApi();
   }, []);
-//   console.log(categories)
-  const ctgs = categories.slice(0,7)
+  //   console.log(categories)
+  const ctgs = categories.slice(0, 7);
 
   return (
-    <div className="cards-row wide">
-      <div className="row-header">
-        <h3 className="category-name">Bạn đang quan tâm gì?</h3>
+    <div className={cx("cards-row" ,"wide")}>
+      <div className={cx("row-header")}>
+        <h3 className={cx("category-name")}>Bạn đang quan tâm gì?</h3>
       </div>
 
-      <div className="row-content">
-        
-        <div className="topics-list">
-            {
-                ctgs.map((categ, index)=>(
-
-          <a href="" className="row-topic">
-            <div className="intro">
-              <div className="heading-md">{categ.name}</div>
-              <div className="info">Xem chủ đề <FontAwesomeIcon icon={faLeftRight}/></div>
-            </div>
-          </a>
-                ))
-            }
-          
+      <div className={cx("row-content")}>
+        <div className={cx("topics-list")}>
+          {ctgs.map((categ, index) => (
+            <a href="" className={cx("row-topic")}>
+              <div className={cx("intro")}>
+                <div className={cx("heading-md")}>{categ.name}</div>
+                <div className={cx("info")}>
+                  Xem chủ đề <FontAwesomeIcon icon={faLeftRight} />
+                </div>
+              </div>
+            </a>
+          ))}
         </div>
       </div>
     </div>
--- src/components/Slide/index.js
+++ src/components/Slide/index.js
@@ -3,25 +3,27 @@ import {
   faHeart,
   faPlay,
 } from "@fortawesome/free-solid-svg-icons";
-import "./Slide.scss";
+import styles from "./Slide.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from 'classnames/bind'
 
 function Slide() {
+  const cx = classNames.bind(styles)
   return (
     <>
-      <div className="top-slide">
+      <div className={cx('top-slide')}>
         <div className="slide-wrapper">
           <img
-            className="img-mask"
+            className={cx("img-mask")}
             src="https://static.nutscdn.com/vimg/1920-0/0ad186e9bbf32f86a48e84a6fbaa94a3.jpg"
             alt=""
           />
         </div>
 
-        <div className="safe-area">
-          <div className="slide-content">
-            <div className="media-item">
-              <div className="media-title-image">
+        <div className={cx("safe-area")}>
+          <div className={cx("slide-content")}>
+            <div className={cx("media-item")}>
+              <div className={cx("media-title-image")}>
                 <a href="">
                   <img
                     alt="Zombie Cưng Của Ba"
@@ -29,85 +31,85 @@ function Slide() {
                   ></img>
                 </a>
               </div>
-              <h3 className="media-title"></h3>
-              <h3 className="title-alias-title">
+              <h3 className={cx("media-title")}></h3>
+              <h3 className={cx("title-alias-title")}>
                 <a href="">My Daughter is a Zombie</a>
               </h3>
-              <div className="hl-tags">
-                <div className="tag-model">
+              <div className={cx("hl-tags")}>
+                <div className={cx("tag-model")}>
                   <span>
                     <strong>T13</strong>
                   </span>
                 </div>
-                <div className="tag-classic">
+                <div className={cx("tag-classic")}>
                   <span>2025</span>
                 </div>
-                <div className="tag-classic">
+                <div className={cx("tag-classic")}>
                   <span>1h 54m</span>
                 </div>
               </div>
-              <div className="hl-tags mb-4">
-                <a className="tag-topic" href="">
+              <div className={cx("hl-tags mb-4")}>
+                <a className={cx("tag-topic")} href="">
                   Chính kịch
                 </a>
-                <a className="tag-topic" href="">
+                <a className={cx("tag-topic")} href="">
                   Hài
                 </a>
               </div>
-              <div className="description">
+              <div className={cx("description")}>
                 Cho Jung Seok hóa thân thành Lee Jung Hwan, một huấn luyện viên
                 động vật đầy nhiệt huyết quyết tâm bảo vệ cô con gái tuổi teen
                 bị nhiễm virus zombie bằng cách “thuần hóa” cô bé.
               </div>
-              <div className="touch">
-                <a href="" className="button-play">
+              <div className={cx("touch")}>
+                <a href="" className={cx("button-play")}>
                   <div>
-                    <FontAwesomeIcon className="icon-play" icon={faPlay} />
+                    <FontAwesomeIcon className={cx("icon-play")} icon={faPlay} />
                   </div>
                 </a>
 
                 {/* Nhóm nút để tương tác */}
-                <div className="touch-group">
-                  <a href="" className="item">
+                <div className={cx("touch-group")}>
+                  <a href="" className={cx("item")}>
                     <FontAwesomeIcon icon={faHeart} />
                   </a>
-                  <a href="" className="item">
+                  <a href="" className={cx("item")}>
                     <FontAwesomeIcon icon={faCircleInfo} />
                   </a>
                 </div>
               </div>
             </div>
-            <div className="swiper">
-              <div className="swiper-wrapper">
-                <div className="swiper-slide">
+            <div className={cx("swiper")}>
+              <div className={cx("swiper-wrapper")}>
+                <div className={cx("swiper-slide")}>
                   <img
                     alt="Xem Phim"
                     loading="lazy"
                     src="https://static.nutscdn.com/vimg/150-0/c986724f99e98514a32f70cecd1cb173.webp"
                   ></img>
                 </div>
-                <div className="swiper-slide">
+                <div className={cx("swiper-slide")}>
                   <img
                     alt="Xem Phim"
                     loading="lazy"
                     src="https://static.nutscdn.com/vimg/150-0/c986724f99e98514a32f70cecd1cb173.webp"
                   ></img>
                 </div>
-                <div className="swiper-slide">
+                <div className={cx("swiper-slide")}>
                   <img
                     alt="Xem Phim"
                     loading="lazy"
                     src="https://static.nutscdn.com/vimg/150-0/c986724f99e98514a32f70cecd1cb173.webp"
                   ></img>
                 </div>
-                <div className="swiper-slide">
+                <div className={cx("swiper-slide")}>
                   <img
                     alt="Xem Phim"
                     loading="lazy"
                     src="https://static.nutscdn.com/vimg/150-0/c986724f99e98514a32f70cecd1cb173.webp"
                   ></img>
                 </div>
-                <div className="swiper-slide">
+                <div className={cx("swiper-slide")}>
                   <img
                     alt="Xem Phim"
                     loading="lazy"
